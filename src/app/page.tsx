679dc53cd261f5fe34fd311a297fb0598f7e8f1c
sajjadth <st.655.st@gmail.com>
2026-09-01 13:29:39 +0330

    feat: integrate Vercel Analytics and Speed Insights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,9 @@ import { ContactSection } from '@/components/contact-section';
 import { Footer } from '@/components/footer';
 import { BackToTop } from '@/components/back-to-top';
 import { EasterEgg } from '@/components/easter-egg';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 export default function Home() {
   return (
@@ -22,6 +25,8 @@ export default function Home() {
       <Footer />
       <BackToTop />
       <EasterEgg />
+      <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
